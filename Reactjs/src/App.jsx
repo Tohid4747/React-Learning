4cd38dd3938b8b10bdff3d560f5c1c3f7c640eb3
Tohid Mulla <tohidmulla444@gmail.com>
2026-08-12 19:40:23 +0530

    learnt the promises , axios and how data is fetched and mapped to show the data  ...also studied how the useEffect works like for rendering , fetching data through api and diffrent functionalites

--- Reactjs/src/App.jsx
+++ Reactjs/src/App.jsx
@@ -62,7 +62,7 @@
 /**  creating counter using React usestate hook
  */
 
-import { useState } from "react";
+// import { useState } from "react";
 
 // const App = () => {
 //   const [num, setNum] = useState(0);
@@ -116,7 +116,7 @@ import { useState } from "react";
 //   const submitHandler = (e) => {
 //     e.preventDefault();//prevent the relaoding of the page when form is submitted
 //     console.log("form submitted", name);
-//     setName("")//cleas the setName after the form is submitted 
+//     setName("")//cleas the setName after the form is submitted
 //   };
 
 //   return (
@@ -148,79 +148,131 @@ import { useState } from "react";
 // const App = () => {
 //   return (
 //     <>
-      
+
 //        <Navbar/>
 //        <Navbar/>
 //        <Footer/>
-       
+
 //     </>
-  
-       
-  
+
 //   )
 // }
 
 // export default App
 
+//creating cards by passing the data using props and props drilling Using Reactjs
+// import React from 'react'
+// import Card from "./components/Card";
 
-import React from 'react'
-import Card from "./components/Card";
+// const App = () => {
+//   const users = [
+//   {
+//     name: "Rahul Sharma",
+//     city: "Pune",
+//     age: 24,
+//     profession: "Software Developer",
+//     profilePhoto: "https://i.pravatar.cc/150?img=1"
+//   },
+//   {
+//     name: "Priya Patil",
+//     city: "Mumbai",
+//     age: 27,
+//     profession: "UI/UX Designer",
+//     profilePhoto: "https://i.pravatar.cc/150?img=2"
+//   },
+//   {
+//     name: "Aman Khan",
+//     city: "Bangalore",
+//     age: 22,
+//     profession: "Web Developer",
+//     profilePhoto: "https://i.pravatar.cc/150?img=3"
+//   },
+//   {
+//     name: "Sneha Joshi",
+//     city: "Delhi",
+//     age: 29,
+//     profession: "Data Analyst",
+//     profilePhoto: "https://i.pravatar.cc/150?img=4"
+//   },
+//   {
+//     name: "Vikram Deshmukh",
+//     city: "Hyderabad",
+//     age: 31,
+//     profession: "Project Manager",
+//     profilePhoto: "https://i.pravatar.cc/150?img=5"
+//   }
+// ];
 
-const App = () => {
-  const users = [
-  {
-    name: "Rahul Sharma",
-    city: "Pune",
-    age: 24,
-    profession: "Software Developer",
-    profilePhoto: "https://i.pravatar.cc/150?img=1"
-  },
-  {
-    name: "Priya Patil",
-    city: "Mumbai",
-    age: 27,
-    profession: "UI/UX Designer",
-    profilePhoto: "https://i.pravatar.cc/150?img=2"
-  },
-  {
-    name: "Aman Khan",
-    city: "Bangalore",
-    age: 22,
-    profession: "Web Developer",
-    profilePhoto: "https://i.pravatar.cc/150?img=3"
-  },
-  {
-    name: "Sneha Joshi",
-    city: "Delhi",
-    age: 29,
-    profession: "Data Analyst",
-    profilePhoto: "https://i.pravatar.cc/150?img=4"
-  },
-  {
-    name: "Vikram Deshmukh",
-    city: "Hyderabad",
-    age: 31,
-    profession: "Project Manager",
-    profilePhoto: "https://i.pravatar.cc/150?img=5"
-  }
-];
+//    return (
+//     <div className="min-h-screen bg-gray-100 p-10 flex flex-wrap justify-center gap-8">
+//       {users.map((user, index) => {
+//         return (
+//           <Card
+//             key={index}
+//             name={user.name}
+//             age={user.age}
+//             city={user.city}
+//             profession={user.profession}
+//             profilePhoto={user.profilePhoto}
+//           />
+//         );
+//       })}
+//     </div>
+//   );
+// }
 
-   return (
-    <div className="min-h-screen bg-gray-100 p-10 flex flex-wrap justify-center gap-8">
-      {users.map((user, index) => {
-        return (
-          <Card
-            key={index}
-            name={user.name}
-            age={user.age}
-            city={user.city}
-            profession={user.profession}
-            profilePhoto={user.profilePhoto}
-          />
-        );
-      })}
-    </div>
-  );
-}
+// export default App
 
-export default App
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+// const App = () => {
+//   const [data, setdata] = useState([]);
+
+//   async function getData() {
+//     const response = await axios.get(
+//       "https://picsum.photos/v2/list?page=2&limit=30",
+//     );
+//     //  console.log(response.data)//to use this data we use state and setstate to store
+//     //  the data and display
+
+//     setdata(response.data);
+//     console.log(data);
+//   }
+
+//   useEffect(() => {
+//     getData(); //useEffect is used to call the function when the component is mounted and also when the state changes
+//   }, []);
+
+//   return (
+//     <div className="p-10">
+//     {/*   <button
+//         onClick={getData}
+//         className="bg-teal-600 text white  font-semibold text-2xl px-6 py-3
+//        rounded active:scale-90"
+//       >
+//         Getdata
+//       </button>
+//        */ }
+//       <div className="p-5 mt-5 bg-blue-950">
+//         {data.map((item, idx) => {
+//           return (
+//             <div
+//               key={idx}
+//               className=" bg-black-50 text-black flex item-center justify-between w-full px-7 py-6 rounded mb-3"
+//             >
+//               <img
+//                 className=" h-40 w-full object-cover"
+//                 src={item.download_url}
+//                 alt=""
+//               />
+//               <h1 className="text-white"> {item.author}</h1>
+//             </div>
+//           );
+//         })}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
