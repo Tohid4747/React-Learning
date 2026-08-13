778022fcd7cf736ab9e4b43d5125292d09d5a2ae
Tohid Mulla <tohidmulla444@gmail.com>
2026-08-14 00:39:13 +0530

    learned how to wrap the app inside any context

--- Reactjs/src/main.jsx
+++ Reactjs/src/main.jsx
@@ -2,15 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'//importing the browser router from react router dom
+import UserContext from './context/UserContext.jsx'
+// import { BrowserRouter } from 'react-router-dom'//importing the browser router from react router dom
 
+
+// createRoot(document.getElementById('root')).render(
+
+//     <BrowserRouter>
+//      <App />
+//      {/* //wrapped the app into Browser Router to use the react router dom */}
+//     </BrowserRouter>
+
+// )
 
 createRoot(document.getElementById('root')).render(
-
-    <BrowserRouter>
-     <App />
-     {/* //wrapped the app into Browser Router to use the react router dom */}
-    </BrowserRouter>
-   
+   <UserContext>
+   <App/>
+   {/* //wrapped the usercontext around the app component to make the data availale to all 
+    so now the app becomes the child of the usercontext */}
+   </UserContext>
 
 )
+
